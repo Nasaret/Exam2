214f13746325f9f0a891169dfe079d2d33922f78
Sylvain Chantepie <sylvain.chantepie28@orange.fr>
2016-11-09 16:58:43 +0100

    Resultat : rien du tout

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 (function(){
 
+	var app = angular.module('exam2', []);
+	app.controller('appController', function(){
+		this.personne = {
+			name:"Tian Bao",
+			age: 2 ,
+			skills: ['Manger', 'Jouer'],
+			image : 'img/panda.jpg'
+		};
+	});
+
+	app.directive('header', function(){
+		return {
+			restrict: 'AE',
+			templateUrl: 'paritials/common/header.html'
+		}
+	})
+	app.directive('footer', function(){
+		return {
+			restrict: 'AE',
+			templateUrl: 'paritials/common/footer.html'
+		}
+	})
+
+	app.directive('home', function(){
+		return {
+			restrict: 'AE',
+			templateUrl: 'paritials/home.html'
+		}
+	})
+})();
 
 
 
-
+/*
 
   var object = {
     name : "Tian Bao",
@@ -12,3 +42,4 @@
     image : 'img/panda.jpg'
   };
 })();
+*/
